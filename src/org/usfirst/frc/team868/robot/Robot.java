fa98e2beea660f2e1523ca19771f8fa1ae57264d
ix <arcetera@openmailbox.org>
2017-09-28 16:54:36 -0400

    deleted example stuff, added ShooterSubsystem

--- src/org/usfirst/frc/team868/robot/Robot.java
+++ src/org/usfirst/frc/team868/robot/Robot.java
@@ -8,8 +8,7 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import org.usfirst.frc.team868.robot.commands.ExampleCommand;
-import org.usfirst.frc.team868.robot.subsystems.ExampleSubsystem;
+// I want die
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,7 +19,7 @@ import org.usfirst.frc.team868.robot.subsystems.ExampleSubsystem;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+
 	public static OI oi;
 
 	Command autonomousCommand;
@@ -32,10 +31,7 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
-		oi = new OI();
-		chooser.addDefault("Default Auto", new ExampleCommand());
-		// chooser.addObject("My Auto", new MyAutoCommand());
-		SmartDashboard.putData("Auto mode", chooser);
+
 	}
 
 	/**
---
+++ src/org/usfirst/frc/team868/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team868.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import com.ctre.CANTalon;
+
+/**
+ *
+ */
+public class ShooterSubsystem extends Subsystem {
+	private final CANTalon shooter; // regret
+
+    public ShooterSubsystem() {
+    	shooter = new CANTalon(0);
+    	
+    	shooter.changeControlMode(CANTalon.TalonControlMode.Voltage);
+    }
+
+    public void setSpeed(double rps) {
+    	
+    }
+    
+    public void setPower(double V) {
+    	shooter.set(V);
+    }
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
